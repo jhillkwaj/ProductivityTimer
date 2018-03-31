f16cccafcfc879067be35f1b8d8a7fe4d0f6a085
Eric Lee <hownl96@gmail.com>
2018-03-31 15:29:23 -0500

    new stuff

--- timer.js
+++ timer.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    console.log("Site Changed Weeb");
+    // Do NOT forget that the method is ASYNCHRONOUS
+    chrome.tabs.query({
+    active: true,
+    currentWindow: true
+    }, function(tabs) {
+    var tabURL = tabs[0].url;
+    console.log(tabURL);
+    $( "p" ).text(tabURL);
+    });
+    
+    
+    //setInterval(function(){ alert("Hello"); }, 1000);
 });
